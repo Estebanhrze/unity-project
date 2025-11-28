797c915b2eb77be996fbf65656a854443aacfa55
Estebanhrze <hectoresteban6972@gmail.com>
2025-11-28 00:13:38 -0500

    new arreglando

--- Scripts/GarbageController.cs
+++ Scripts/GarbageController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GarbageController : MonoBehaviour{
     void OnTriggerEnter2D(Collider2D collision){
-        Destroy(collision.gameObject);  
+        // Solo destruye objetos con tag "Platform"
+        if(collision.gameObject.CompareTag("Platform")){
+            Destroy(collision.gameObject);
+        }
     }
 }
